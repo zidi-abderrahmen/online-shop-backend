be68fddab9732eed13190fec5c32439f3f5a8638
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-15 14:19:15 +0100

    feat: add the User repository interface with basic email finder operations

---
+++ src/main/java/com/na/store/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.na.store.repositories;
+
+import com.na.store.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
