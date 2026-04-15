07087844219df21768ba37bced51ff445d708d6c
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-15 14:41:37 +0100

    feat: added the User register request mapper to handle entity to dto and dto to entity conversions

---
+++ src/main/java/com/na/store/mappers/UserRequestMapper.java
@@ -0,0 +1,16 @@
+package com.na.store.mappers;
+
+import com.na.store.dtos.UserRegisterRequest;
+import com.na.store.entities.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserRequestMapper {
+
+    UserRegisterRequest toDto(User user);
+
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    User toEntity(UserRegisterRequest userRegisterRequest);
+}
