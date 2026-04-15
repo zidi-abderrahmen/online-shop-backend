2b182674b33f26254b28d3055967871dcae296de
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-15 15:37:16 +0100

    feat: added Auth controller with user registration

---
+++ src/main/java/com/na/store/controllers/AuthController.java
@@ -0,0 +1,23 @@
+package com.na.store.controllers;
+
+import com.na.store.dtos.UserRegisterRequest;
+import com.na.store.dtos.UserRegisterResponse;
+import com.na.store.services.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserRegisterResponse register(@Valid @RequestBody UserRegisterRequest request) {
+        return authService.createNewUser(request);
+    }
+}
