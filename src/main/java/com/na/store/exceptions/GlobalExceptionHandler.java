3f8982f6b9afde92fb1940195c8f129879af180a
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-15 14:49:34 +0100

    feat: added email already exists exception in the global exception handler

---
+++ src/main/java/com/na/store/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.na.store.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<Object> handleEmailAlreadyExistsException(EmailAlreadyExistsException e) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", e.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+}
