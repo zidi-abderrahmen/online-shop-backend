df4d10c81661e177f58f2a373815b88046a949b0
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-15 15:31:16 +0100

    feat: added Auth service with user registration

---
+++ src/main/java/com/na/store/services/AuthService.java
@@ -0,0 +1,43 @@
+package com.na.store.services;
+
+import com.na.store.dtos.UserRegisterRequest;
+import com.na.store.dtos.UserRegisterResponse;
+import com.na.store.entities.User;
+import com.na.store.exceptions.EmailAlreadyExistsException;
+import com.na.store.exceptions.PasswordMismatchException;
+import com.na.store.mappers.UserResponseMapper;
+import com.na.store.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserResponseMapper userResponseMapper;
+
+    @Transactional
+    public UserRegisterResponse createNewUser(UserRegisterRequest request) {
+        if (userRepository.existsByEmail(request.email())) {
+            throw new EmailAlreadyExistsException("Email already exists");
+        }
+
+        if (!request.password().equals(request.confirmPassword())) {
+            throw new PasswordMismatchException("Passwords do not match");
+        }
+
+        User newUser = User.builder()
+                .name(request.name())
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .build();
+
+        User savedUser = userRepository.save(newUser);
+
+        return userResponseMapper.toDto(savedUser);
+    }
+}
