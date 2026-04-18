585926c7eddd703bb95d87d86273b79121de02fe
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-19 00:33:56 +0100

    feat: added @EnableMethodSecurity to enable method security for admin controller

--- src/main/java/com/na/store/controllers/CreateAdminController.java
+++ src/main/java/com/na/store/controllers/CreateAdminController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class CreateAdminController {
 
     private final CreateAdminService createAdminService;
