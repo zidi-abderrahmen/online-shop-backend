3e1612a0adb10bf24bede8dd55909ae3a7176283
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-18 21:09:18 +0100

    feat: added CustomUserDetailsService for user authentication and JWT handling

---
+++ src/main/java/com/na/store/services/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.na.store.services;
+
+import com.na.store.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override @NonNull
+    public UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
