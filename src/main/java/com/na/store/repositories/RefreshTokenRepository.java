92fb2c20d62f0aa5b602c66e24536c6084730954
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-19 21:29:32 +0100

    feat: added refresh token repository

---
+++ src/main/java/com/na/store/repositories/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package com.na.store.repositories;
+
+import com.na.store.entities.RefreshToken;
+import com.na.store.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUser(User user);
+}
