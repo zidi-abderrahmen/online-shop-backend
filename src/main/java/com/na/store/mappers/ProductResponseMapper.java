46c69adfe2a33475dece8c67d5c4a524922a11f2
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-25 22:24:53 +0100

    feat: added another toDto map structure

--- src/main/java/com/na/store/mappers/ProductResponseMapper.java
+++ src/main/java/com/na/store/mappers/ProductResponseMapper.java
@@ -1,13 +1,19 @@
 package com.na.store.mappers;
 
 import com.na.store.dtos.product.ProductResponse;
+import com.na.store.dtos.product.images.ProductImageResponse;
 import com.na.store.entities.Product;
+import com.na.store.entities.ProductImages;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductResponseMapper {
 
+    @Mapping(target = "imagesUrl", source = "imagesUrl")
     ProductResponse toDto(Product product);
+
+    ProductImageResponse toDto(ProductImages productImages);
 
     Product toEntity(ProductResponse productResponse);
 }
