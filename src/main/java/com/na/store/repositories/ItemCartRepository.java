b4f743ddd28bd4733e6691a641740cbf694bd01f
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-29 23:13:16 +0100

    feat: added ItemCartRepository interface with methods for user cart management

---
+++ src/main/java/com/na/store/repositories/ItemCartRepository.java
@@ -0,0 +1,19 @@
+package com.na.store.repositories;
+
+import com.na.store.entities.ItemCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemCartRepository extends JpaRepository<ItemCart, Long> {
+
+    List<ItemCart> findByUserId(String userId);
+
+    @Query("SELECT ic FROM ItemCart ic WHERE ic.user.email = :email AND ic.product.id = :productId")
+    Optional<ItemCart> findCartItem(@Param("email") String email, @Param("productId") Long productId);
+
+    void deleteByUserId(String userId);
+}
